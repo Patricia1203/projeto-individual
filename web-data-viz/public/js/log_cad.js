822dc7f3dbd6dcabedbafb072a87ca0822fbef11
Patricia1203 <patricia.borges@sptech.school>
2025-06-04 01:30:34 -0300

    Modificado a insertção dos dados no Banco de Dados para permitir o nickname ser NULL

--- web-data-viz/public/js/log_cad.js
+++ web-data-viz/public/js/log_cad.js
@@ -18,7 +18,7 @@ btnSignup.addEventListener("click", function () {
 });
 
 document.querySelectorAll('.nav-button-box a').forEach(link => {
-    link.addEventListener('click', function(e) {
+    link.addEventListener('click', function (e) {
         if (window.location.pathname.endsWith('login_cad.html')) {
             e.preventDefault();
             const mode = this.href.includes('mode=cadastro') ? 'sign-in-js' : 'sign-up-js';
@@ -74,10 +74,84 @@ if (senhaInput) {
     });
 }
 
-//Login
 
+// Cadastro
+function cadastrar() {
+
+    //Recupere o valor da nova input pelo nome do id
+    // Agora vá para o método fetch logo abaixo
+    var nomeVar = nome_input.value;
+    var emailVar = email_input.value;
+    var nicknameVar = nickname_input.value;
+    var senhaVar = senha_input.value;
+    var confirmacaoSenhaVar = confirmacao_senha_input.value;
+
+    
+    // Verificando se o nickname é vazio e atribuindo null se ele for.
+    var nicknameToSend = nicknameVar === "" ? null : nicknameVar;
+
+
+    // Verificando se há algum campo em branco
+    if (
+        nomeVar == "" ||
+        emailVar == "" ||
+        senhaVar == "" ||
+        confirmacaoSenhaVar == ""
+    ) {
+        alert('Mensagem de erro para todos os campos em branco');
+        return;
+    }
+
+    // Verificando se a senha e a confirmação de senha são iguais
+    if (senhaVar != confirmacaoSenhaVar) {
+        alert('As senhas não são iguais, por favor verifique');
+
+        return;
+    }
+
+
+    // Enviando o valor da nova input
+    fetch("/usuarios/cadastrar", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+            // crie um atributo que recebe o valor recuperado aqui
+            // Agora vá para o arquivo routes/usuario.js
+            nomeServer: nomeVar,
+            nicknameServer: nicknameToSend,
+            emailServer: emailVar,
+            senhaServer: senhaVar,
+        }),
+    })
+        .then(function (resposta) {
+            console.log("resposta: ", resposta);
+
+            if (resposta.ok) {
+
+                alert("Cadastro realizado com sucesso! Redirecionando para tela de Login...");
+
+                /* setTimeout(() => {
+                  window.location = "login.html";
+                }, "2000"); */
+
+            } else {
+                throw "Houve um erro ao tentar realizar o cadastro!";
+                alert("Houve um erro ao tentar realizar o cadastro!");
+            }
+        })
+        .catch(function (resposta) {
+            console.log(`#ERRO: ${resposta}`);
+        });
+
+    return;
+}
+
+
+//Login
 function entrar(event) {
-    if (event) event.preventDefault(); 
+    if (event) event.preventDefault();
 
 
     var emailVar = document.getElementById('login_email_input').value;
@@ -118,74 +192,5 @@ function entrar(event) {
         console.log(erro);
     });
 
-    return ;
-}
-
-// Cadastro
-function cadastrar() {
-
-    //Recupere o valor da nova input pelo nome do id
-    // Agora vá para o método fetch logo abaixo
-    var nomeVar = nome_input.value;
-    var emailVar = email_input.value;
-    var nicknameVar = nickname_input.value;
-    var senhaVar = senha_input.value;
-    var confirmacaoSenhaVar = confirmacao_senha_input.value;
-
-
-    // Verificando se há algum campo em branco
-    if (
-      nomeVar == "" ||
-      emailVar == "" ||
-      nicknameVar == "" ||
-      senhaVar == "" ||
-      confirmacaoSenhaVar == ""
-    ) {
-       alert('Mensagem de erro para todos os campos em branco');
-      return;
-    }
-
-    // Verificando se a senha e a confirmação de senha são iguais
-    if (senhaVar != confirmacaoSenhaVar) {
-      alert('As senhas não são iguais, por favor verifique'); 
-
-      return;
-    } 
-
-
-    // Enviando o valor da nova input
-    fetch("/usuarios/cadastrar", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        // crie um atributo que recebe o valor recuperado aqui
-        // Agora vá para o arquivo routes/usuario.js
-        nomeServer: nomeVar,
-        nicknameServer: nicknameVar,
-        emailServer: emailVar,
-        senhaServer: senhaVar,
-      }),
-    })
-      .then(function (resposta) {
-        console.log("resposta: ", resposta);
-
-        if (resposta.ok) {
-
-          alert("Cadastro realizado com sucesso! Redirecionando para tela de Login...");
-
-          /* setTimeout(() => {
-            window.location = "login.html";
-          }, "2000"); */
-
-        } else {
-          throw "Houve um erro ao tentar realizar o cadastro!";
-          alert("Houve um erro ao tentar realizar o cadastro!"); }
-      })
-      .catch(function (resposta) {
-        console.log(`#ERRO: ${resposta}`);
-      });
-
     return;
-  }
+}
