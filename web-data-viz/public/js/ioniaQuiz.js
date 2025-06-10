0ce24379e09279cc983e0422113b9df9b633f085
Patricia1203 <patricia.borges@sptech.school>
2025-06-10 18:16:19 -0300

    Adicionando o quiz de Ionia

---
+++ web-data-viz/public/js/ioniaQuiz.js
@@ -0,0 +1,32 @@
+const id_quiz = 4; 
+let listaDeQuestoes = [];
+
+async function carregarQuestoes() {
+    try {
+        const resp = await fetch(`/quizzes/perguntas/${id_quiz}`);
+    
+        
+        const questoesRecebidas = await resp.json();
+
+        listaDeQuestoes = questoesRecebidas.map(q => ({
+            pergunta: q.pergunta,
+            alternativaA: q.alternativa_a,
+            alternativaB: q.alternativa_b,
+            alternativaC: q.alternativa_c,
+            alternativaD: q.alternativa_d,
+            alternativaCorreta: q.alternativaCorreta
+        }));
+
+        totalQuestoes = listaDeQuestoes.length;
+        if (totalQuestoes === 0) {
+            console.log("Nenhuma questão encontrada para o quiz.");
+
+        } else {
+            iniciarQuizRegiao(); 
+        }
+
+    } catch (error) {
+        console.error("Falha ao carregar as questões:", error);
+    }
+}
+
